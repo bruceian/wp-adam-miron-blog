02b0665c52ac1c4d29d68298d14b5615bdc8f0ad
Ian Bruce <brucegordonian@gmail.com>
2019-02-10 12:24:44 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+  <footer>
+
+
+  </footer>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js" type="text/javascript"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html class="no-js supports-no-cookies" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0'">
+    <meta name="theme-color" content="#0c3520">
+    <link rel="canonical" href="">
+    <link rel="shortcut icon" href="" type="image/png">
+    <title>Adam Miron</title>
+    <meta name="description" content="">
+
+    <meta property="og:site_name" content="Adam Miron Blog">
+    <meta property="og:url" content="">
+    <meta property="og:title" content="Adam Miron Blog">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Adam Miron's blog">
+    <meta property="og:image" content=""/>
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
+
+
+    <meta name="twitter:site" content="@">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Adam Miron">
+    <meta name="twitter:description" content="Adam Miron's twitter">
+</head>
+  <body>
+
+    <header>
+
+
+
+    </header>
